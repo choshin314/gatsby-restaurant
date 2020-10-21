cc1b8fe865d2175ab3311f650ef0fd1b601d397b
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-10-21 12:13:18 -0500

    add generic card container to style lib

--- src/components/styled-lib/index.js
+++ src/components/styled-lib/index.js
@@ -20,3 +20,24 @@ export const GridContainer = styled.div`
     grid-template-columns: ${props => props.columns};
     grid-gap: 1rem;
 `
+
+export const Card = styled.div`
+    box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+    transition: transform .2s ease-in-out;
+    &::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+        box-shadow: 0 5px 15px rgba(0,0,0,0.3);
+        opacity: 0;
+        transition: opacity 0.3s ease-in-out;
+    }
+    &:hover {
+        transform: scale(1.05);
+    }
+    &:hover::after {
+        opacity: 1;
+    }
+`
