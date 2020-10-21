029827c6c7a45b72c7d2c740cb8f47a231cb3445
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-10-21 12:02:11 -0500

    add generic grid container to style lib

--- src/components/styled-lib/index.js
+++ src/components/styled-lib/index.js
@@ -9,9 +9,14 @@ export const devices = {
 }
 
 export const Wrapper = styled.div`
-    width: 100%;
+    width: 90%;
     max-width: 1200px;
     padding: 1rem;
     margin: 0 auto;
     position: relative;
+`
+export const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: ${props => props.columns};
+    grid-gap: 1rem;
 `
