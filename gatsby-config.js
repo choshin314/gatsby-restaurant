e24ec286cc2a64635e46a55ade661261ab95b154
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-10-25 22:21:09 -0500

    dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,22 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
   plugins: [
+    'gatsby-plugin-styled-components',
     `gatsby-transformer-sharp`, 
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'vq3awelcvugd',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    }
   ],
 }
